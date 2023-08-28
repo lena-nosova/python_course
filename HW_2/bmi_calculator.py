9ff69f1d49813611411e9a72667c672f98d06ce0
LN <l.nosova16@yandex.ru>
2023-08-28 18:55:00 +0300

    changing the variable name

--- HW_2/bmi_calculator.py
+++ HW_2/bmi_calculator.py
@@ -1,12 +1,12 @@
 the_height = float(input("Введите Ваш рост (м): "))
 the_weight = float(input("Введите Ваш вес (кг): "))
-bim = the_weight/(the_height**2)
-print("Ваш индекс массы тела:", bim)
+bmi = the_weight/(the_height**2)
+print("Ваш индекс массы тела:", bmi)
 
 a = 16
 b = 40
-b1 = int(bim) - a
-b2 = b - int(bim)
+b1 = int(bmi) - a
+b2 = b - int(bmi)
 
 print("16" + b1 * "=" + "|" + b2 * "=" + "40")
 
