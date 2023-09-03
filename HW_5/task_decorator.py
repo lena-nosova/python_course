a45cc298ac5cb1eaab68fb609a1206be79d3c9d4
LN <l.nosova16@yandex.ru>
2023-09-03 21:40:06 +0300

    HW_5

---
+++ HW_5/task_decorator.py
@@ -0,0 +1,19 @@
+import functools
+
+login = (input("Введите логин: "))
+admin_users = {"Петя": 'admin'}
+
+def check_login(func):
+    @functools.wraps(func)
+    def wrapper_decorator(*args, **kwargs):
+        if login in admin_users:
+            print(login, sum)
+            value = func(*args, **kwargs)
+        return value
+    return wrapper_decorator
+
+@check_login
+def not_acsess():
+    if login not in admin_users:
+        return "Доступ запрещен"
+    
