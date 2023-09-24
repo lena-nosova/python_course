e5e84e3afb9fb8d654b3418b3387c9134d09692e
LN <l.nosova16@yandex.ru>
2023-09-24 11:57:48 +0300

    new

--- src/random_number/views.py
+++ src/random_number/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from random import randint
 
 # Create your views here.
+
+def get_random(request):
+    return(HttpResponse("<h2>Result</h2>" + str(randint(0, 100000))))
+    
+
